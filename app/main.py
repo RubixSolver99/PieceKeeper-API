375fe6c48c2e3cb8c516c3fbed77b92472daeae7
RubixSolver99 <blackbattiger@outlook.com>
2024-07-05 15:47:03 -0500

    EDITED

--- app/main.py
+++ app/main.py
@@ -1,13 +1,7 @@
-from typing import Annotated
-
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-@app.get("/items/")
-async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+@app.get('/')
+async def root():
+        return {'message': 'Hello World'}
